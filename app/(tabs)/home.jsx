639bc68d15eea4fdc1540baf1016647322f73023
Anasser69 <ahmed.timo189@gmail.com>
2024-07-31 17:01:40 +0300

    Make Trending videos and refresh feature with empty state

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,14 +1,22 @@
-import { View, Text, FlatList, Image } from "react-native";
-import React from "react";
+import { View, Text, FlatList, Image, RefreshControl } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
+import EmptyState from "../../components/EmptyState";
 const Home = () => {
+  const[refreshing,setRefreshing]=useState(false)
+
+  const onRefresh = async ()=>{
+    setRefreshing(true);
+    setRefreshing(false)
+  }
   return (
-    <SafeAreaView className=" bg-primary">
+    <SafeAreaView className=" bg-primary h-full">
       <FlatList
         data={[{ id: 1 }]}
+        // data={[]}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
           <Text className="text-3xl text-white">{item.id}</Text>
@@ -42,6 +50,13 @@ const Home = () => {
           </View>
           </View>
         )}
+        ListEmptyComponent={()=>(
+          <EmptyState
+          title='No Videos Found'
+          subtitle="Be The First One To Uplode Videos"
+          />
+        )}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
       />
     </SafeAreaView>
   );
